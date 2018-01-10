cd7df20a99fd7a7d5e9b7e1b55b09f29fd7a557f
Guest User <Guest@epicodus-25.local>
2018-01-09 16:24:19 -0800

    Created all files and folders, figured out toggle jQuery functionality for flashcards

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+$(function(){
+
+
+  $("#expand1").click(function(){
+  $(".js").toggle();
+  });
+
+  $("#expand2").click(function(){
+  $(".optrs").toggle();
+  });
+
+  $("#expand3").click(function(){
+  $(".var").toggle();
+  });
+
+
+
+});
