5ab8e0efbac314282f83234ab0ba40a98a39d207
Guest User <Guest@epicodus-25.local>
2018-01-09 17:21:33 -0800

    change code for efficiency

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,5 @@
-$(function(){
-
-
-  $("#expand1").click(function(){
-  $(".js").toggle();
-  });
-
-  $("#expand2").click(function(){
-  $(".optrs").toggle();
-  });
-
-  $("#expand3").click(function(){
-  $(".var").toggle();
-  });
-
-
-
+$(document).ready(function() {
+$(".expand").click(function(){
+$(this).children("p").toggle();
+});
 });
